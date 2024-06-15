b98da8570d258635d9f310279886c8010a3f0d00
Daniel Vargas García <dvg9383@gmail.com>
2024-06-15 19:32:06 +0200

    entregable4js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,32 @@
-const sections = document.querySelectorAll("[data-src]");
-for (const section of sections) {
-  const response = await fetch(section.dataset.src); // cogemos todos las secciones con el dataset-src de la main
-  const mainContent = await response.text();  // pasamos la respuesta obtenida de cada archivo a texto plano
-  section.outerHTML = mainContent;        // la mostramos por el navegador sustituyendo cada seccion por su                                         archivo referenciado en el data-src
+//lazy load
+
+const sections = document.querySelectorAll("[data-src]");  
+for (const section of sections) {           
+   const response = await fetch(section.dataset.src);  
+   const mainContent = await response.text();  
+   section.outerHTML = mainContent;
 }
 
-const form = document.querySelector("form");
-console.log(form);
-form.addEventListener("submit", function (event) {
-  let email = document.getElementById("inputEmail").value;
-  let phone = document.getElementById("inputPhone").value;
-  let emailPattern = /@gmail\./i;
-  if (emailPattern.test(email)) {
-    alert("Your address can not be @gmail ❌❌❌❌");
-    event.preventDefault();
-  }
-  let phonePattern = /^[679]\d{8}(\s.*)?$/;
-  if (!phonePattern.test(phone)) {
-    alert("Your phone number is not valid ❌❌❌❌");
-    event.preventDefault();
-  }
+//validaciones
 
-});
+  inputEmail.addEventListener("blur", function () {   
+    let email = document.getElementById("inputEmail").value;  
+    let emailPattern = /@gmail\./i;     
+    if (emailPattern.test(email)) {  
+      emailWarning.style.display = 'block';   
+    } else {
+      emailWarning.style.display = 'none';  
+    }
+  });
+
+  inputPhone.addEventListener("blur", function () {  
+    let phone = document.getElementById("inputPhone").value;
+    let phonePattern = /^[679]\d{8}(\s.*)?$/;
+    if (!phonePattern.test(phone) && phone) {
+      phoneWarning.style.display = 'block';
+    } else {
+      phoneWarning.style.display = 'none';
+    }
+  });
+
+
