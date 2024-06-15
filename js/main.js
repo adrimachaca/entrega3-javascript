3df8498695c005b7056c719606d90555fb8f44ca
Daniel Vargas García <161933152+DaniVG7@users.noreply.github.com>
2024-06-15 13:33:44 +0200

    form.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@ for (const section of sections) {
   section.outerHTML = mainContent;        // la mostramos por el navegador sustituyendo cada seccion por su                                         archivo referenciado en el data-src
 }
 
+const form = document.querySelector("form");
+console.log(form);
+form.addEventListener("submit", function (event) {
+  let email = document.getElementById("inputEmail").value;
+  let phone = document.getElementById("inputPhone").value;
+  let emailPattern = /@gmail\./i;
+  if (emailPattern.test(email)) {
+    alert("Your address can not be @gmail ❌❌❌❌");
+    event.preventDefault();
+  }
+  let phonePattern = /^[679]\d{8}(\s.*)?$/;
+  if (!phonePattern.test(phone)) {
+    alert("Your phone number is not valid ❌❌❌❌");
+    event.preventDefault();
+  }
+
+});
